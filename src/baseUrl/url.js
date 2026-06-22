da0f27e20caf0a3d0dc86c9e61d059ab7f57624a
Reza Saputra <reza@femaledaily.com>
2026-06-22 15:22:34 +0700

    improve request body

--- src/baseUrl/url.js
+++ src/baseUrl/url.js
@@ -1,9 +1,9 @@
 export const url = {
-    dev : 'https://dev-postman-echo.net',
-    canary : 'https://canary-postman-echo.com',
-    prod : 'https://postman-echo.com'
+    dev: 'https://postman-echo.com',
+    canary: 'https://canary-postman-echo.com',
+    prod: 'https://postman-echo.com'
 }
 
-export function baseUrl(env){
+export function baseUrl(env) {
     return url[env] || url.dev // Default ke 'dev' jika env tidak dikenal
-}
+} 
